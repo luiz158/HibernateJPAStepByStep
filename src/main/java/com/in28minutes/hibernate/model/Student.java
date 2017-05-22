fc804ceac08bb6e26c8bbf0b683c94d2af198347
Luiz Augusto <luiz158@gmail.com>
2017-05-22 19:23:59 -0300

    m6 - sources....

---
+++ src/main/java/com/in28minutes/hibernate/model/Student.java
@@ -0,0 +1,63 @@
+package com.in28minutes.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String name;
+
+	@OneToOne
+	private Passport passport;
+
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Passport getPassport() {
+		return passport;
+	}
+
+	public void setPassportId(Passport passport) {
+		this.passport = passport;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", passport=" + passport + ", email=" + email + "]";
+	}
+
+}
+
