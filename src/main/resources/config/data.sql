e69712adc2c098fd4fd690f299ee1273aa5d17ff
Luiz Augusto <luiz158@gmail.com>
2017-05-22 21:17:45 -0300

    m9 igual m8, m7 e m6....

---
+++ src/main/resources/config/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO passport VALUES (201,'L1234567','India');
+
+INSERT INTO student VALUES (101,201,'Jane', 'jane@doe.com');
+
+INSERT into project VALUES (301, 'In28Minutes Project 1');
+
+INSERT into task VALUES (401, 301, 'Create JPA Tutorial','2015-12-24');
+
+INSERT into student_project VALUES (501, 101, 301, 401);
+
